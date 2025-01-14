d0cc400cf1a5cdf0f2e76cbac527b0829af98176
個人用 <52556292+hsx124@users.noreply.github.com>
2025-01-14 22:51:32 +0900

    Delete src directory

--- src/main/java/cn/hsx124/server/servlet/Servlet.java
+++
@@ -1,5 +0,0 @@
-package cn.hsx124.server.servlet;
-
-public interface Servlet {
-	public void service(ServletResponse response);
-}
--- src/main/java/cn/hsx124/server/servlet/ServletResponse.java
+++
@@ -1,9 +0,0 @@
-package cn.hsx124.server.servlet;
-
-import java.io.PrintWriter;
-
-public interface ServletResponse {
-	public void setWriter(PrintWriter out);
-
-	public PrintWriter getWriter();
-}
